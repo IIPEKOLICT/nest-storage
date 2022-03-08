5576faacc2a33bf497b8347101ce86468e9bb85f
iipekolict <iipekolict@gmail.com>
2022-03-08 20:28:35 +0300

    refactor: some fixes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,17 +8,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Get all dirs' })
+  @ApiResponse({ type: [String] })
+  @Get()
+  getDirs(): Observable<string[]> {
+    return this.appService.getDirs();
+  }
+
   @ApiOperation({ summary: 'Get dir content' })
   @ApiResponse({ type: [String] })
   @Get(':dirName')
   getDirContent(@Param('dirName') dirName: string): Observable<string[]> {
     return this.appService.getDirContent(dirName || '');
-  }
-
-  @ApiOperation({ summary: 'Get all dirs' })
-  @ApiResponse({ type: [String] })
-  @Get('dirs')
-  getDirs(): Observable<string[]> {
-    return this.appService.getDirs();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const port: string | number = process.env.PORT || LOCAL_PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(port, () => console.log(`Server started on port ${port}`));
 }
 bootstrap();
